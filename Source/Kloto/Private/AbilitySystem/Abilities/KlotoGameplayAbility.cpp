41cfe6f3425dcf56d7abbe89ef4eb41541dd006c
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-11-26 13:49:22 +0800

    Ability Activation Policy

---
+++ Source/Kloto/Private/AbilitySystem/Abilities/KlotoGameplayAbility.cpp
@@ -0,0 +1,34 @@
+// Paradise_NiseMono all rights reserved
+
+
+#include "AbilitySystem/Abilities/KlotoGameplayAbility.h"
+
+#include "AbilitySystemComponent.h"
+
+void UKlotoGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	if (ActivationPolicy == EKlotoAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo && !Spec.IsActive())
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
+
+void UKlotoGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	if (ActivationPolicy == EKlotoAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
+}
---
+++ Source/Kloto/Public/AbilitySystem/Abilities/KlotoGameplayAbility.h
@@ -0,0 +1,32 @@
+// Paradise_NiseMono all rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "KlotoGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class EKlotoAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
+/**
+ * 
+ */
+UCLASS()
+class KLOTO_API UKlotoGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	//~ Begin UGameplayAbility	Interface
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	//~ End UGameplayAbility Interface
+	
+	UPROPERTY(EditDefaultsOnly, Category = "KlotoAbility")
+	EKlotoAbilityActivationPolicy ActivationPolicy = EKlotoAbilityActivationPolicy::OnTriggered;
+};
