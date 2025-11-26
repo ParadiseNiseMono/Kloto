9cae425b0d67a4374faf4e09dc94c0b2ebe30856
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-11-26 11:45:20 +0800

    Robot Anim Instance

--- Source/Kloto/Private/AnimInstances/Robot/KlotoRobotAnimInstance.cpp
+++ Source/Kloto/Private/AnimInstances/Robot/KlotoRobotAnimInstance.cpp
@@ -3,3 +3,29 @@
 
 #include "AnimInstances/Robot/KlotoRobotAnimInstance.h"
 
+#include "Characters/KlotoRobotCharacter.h"
+
+void UKlotoRobotAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	if (OwingCharacter)
+	{
+		OwingRobotCharacter = Cast<AKlotoRobotCharacter>(OwingCharacter);
+	}
+}
+
+void UKlotoRobotAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElpasedTime += DeltaSeconds;
+		bShouldEnterRelaxState = (IdleElpasedTime >= EnterRelaxStateThreshold);
+	}
+}
--- Source/Kloto/Public/AnimInstances/Robot/KlotoRobotAnimInstance.h
+++ Source/Kloto/Public/AnimInstances/Robot/KlotoRobotAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/KlotoCharacterAnimInstance.h"
 #include "KlotoRobotAnimInstance.generated.h"
 
+class AKlotoRobotCharacter;
 /**
  * 
  */
@@ -13,5 +14,20 @@ UCLASS()
 class KLOTO_API UKlotoRobotAnimInstance : public UKlotoCharacterAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AnimData|References")
+	AKlotoRobotCharacter* OwingRobotCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+	float IdleElpasedTime;
 };
