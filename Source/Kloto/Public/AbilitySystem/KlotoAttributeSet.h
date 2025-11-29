e66d80e878ef12a9db12689b3499e43d1978b7fb
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-11-29 12:58:49 +0800

    Gameplay Effect

--- Source/Kloto/Public/AbilitySystem/KlotoAttributeSet.h
+++ Source/Kloto/Public/AbilitySystem/KlotoAttributeSet.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystem/KlotoAbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "KlotoAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +20,32 @@ UCLASS()
 class KLOTO_API UKlotoAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	UKlotoAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UKlotoAttributeSet, CurrentHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UKlotoAttributeSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UKlotoAttributeSet, CurrentRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UKlotoAttributeSet, MaxRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UKlotoAttributeSet, AttackPower)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UKlotoAttributeSet, DefensePower)
+
 };
