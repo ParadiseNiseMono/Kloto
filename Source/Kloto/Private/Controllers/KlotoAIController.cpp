40b2b9188f376e5651f70e0642647333af72d808
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-12-03 16:25:28 +0800

    AI Perception

--- Source/Kloto/Private/Controllers/KlotoAIController.cpp
+++ Source/Kloto/Private/Controllers/KlotoAIController.cpp
@@ -5,6 +5,8 @@
 
 #include "KlotoDebugHelper.h"
 #include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 AKlotoAIController::AKlotoAIController(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
@@ -13,4 +15,22 @@ AKlotoAIController::AKlotoAIController(const FObjectInitializer& ObjectInitializ
 	{
 		Debug::Print(TEXT("CrowdFollowingComponent Valid!"));
 	}
+
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("AISenseConfig_Sight"));
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	AISenseConfig_Sight->SightRadius = 5000.f;
+	AISenseConfig_Sight->LoseSightRadius = 0.f;
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("EnemyPerceptionComponent"));
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnEnemyPerceptionUpdated);
+}
+
+void AKlotoAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	
 }
--- Source/Kloto/Public/Controllers/KlotoAIController.h
+++ Source/Kloto/Public/Controllers/KlotoAIController.h
@@ -6,6 +6,9 @@
 #include "AIController.h"
 #include "KlotoAIController.generated.h"
 
+struct FAIStimulus;
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
 /**
  * 
  */
@@ -14,5 +17,16 @@ class KLOTO_API AKlotoAIController : public AAIController
 {
 	GENERATED_BODY()
 
+public:
 	AKlotoAIController(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* EnemyPerceptionComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* AISenseConfig_Sight;
+
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
 };
