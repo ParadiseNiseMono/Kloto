6b560246af0f5992c4a1eb499bebeab025f039bc
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-12-05 16:13:51 +0800

    Toggle Strafing State

--- Source/Kloto/Public/KlotoGameplayTags.h
+++ Source/Kloto/Public/KlotoGameplayTags.h
@@ -38,6 +38,8 @@ namespace KlotoGameplayTags
 	
 	KLOTO_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Weapon);
 
+	KLOTO_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Status_Strafing);
+
 	/*Shared Tags*/
 	KLOTO_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Shared_Ability_HitReact);
 	KLOTO_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Shared_Ability_Death);
