94233a7f190cd5553e16cb7f604a42dd9a188ca3
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-12-17 11:40:12 +0800

    On Ability Icon Updated

--- Source/Kloto/Public/KlotoTypes/KlotoStructTypes.h
+++ Source/Kloto/Public/KlotoTypes/KlotoStructTypes.h
@@ -31,7 +31,7 @@ struct FKlotoRobotSpecialAbilitySet : public FKlotoRobotAbilitySet
 	GENERATED_BODY()
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TSoftObjectPtr<UMaterialInterface> AbilityIconMaterial;
+	TSoftObjectPtr<UMaterialInterface> SoftAbilityIconMaterial;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "Player.Cooldown"))
 	FGameplayTag AbilityCooldownTag;
