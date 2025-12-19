e4498b29927da8ac9fbe858dc5cb1e3849545452
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-12-19 15:10:28 +0800

    On Stone Being Overlap

--- Source/Kloto/Private/Items/PickUps/KlotoStoneBase.cpp
+++ Source/Kloto/Private/Items/PickUps/KlotoStoneBase.cpp
@@ -3,3 +3,18 @@
 
 #include "Items/PickUps/KlotoStoneBase.h"
 
+#include "KlotoGameplayTags.h"
+#include "AbilitySystem/KlotoAbilitySystemComponent.h"
+#include "Characters/KlotoRobotCharacter.h"
+
+void AKlotoStoneBase::OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                                          UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnPickUpCollisionSphereBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep,
+	                                           SweepResult);
+
+	if (AKlotoRobotCharacter* OverlappedCharacter = Cast<AKlotoRobotCharacter>(OtherActor))
+	{
+		OverlappedCharacter->GetKlotoAbilitySystemComponent()->TryActivateAbilityByTag(KlotoGameplayTags::Player_Ability_PickUp_Stones);
+	}
+}
--- Source/Kloto/Public/Items/PickUps/KlotoStoneBase.h
+++ Source/Kloto/Public/Items/PickUps/KlotoStoneBase.h
@@ -13,5 +13,7 @@ UCLASS()
 class KLOTO_API AKlotoStoneBase : public AKlotoPickUpBase
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 };
