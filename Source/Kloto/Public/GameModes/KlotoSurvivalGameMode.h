96d4efce6c40b38d75b7dbdab1135bdaba94af1b
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-12-20 11:02:31 +0800

    Enemy Wave Spawner Data Table

--- Source/Kloto/Public/GameModes/KlotoSurvivalGameMode.h
+++ Source/Kloto/Public/GameModes/KlotoSurvivalGameMode.h
@@ -6,6 +6,8 @@
 #include "GameModes/KlotoBaseGameMode.h"
 #include "KlotoSurvivalGameMode.generated.h"
 
+class AKlotoEnemyCharacter;
+
 UENUM(BlueprintType)
 enum class EKlotoSurvivalGameModeState : uint8
 {
@@ -15,6 +17,33 @@ enum class EKlotoSurvivalGameModeState : uint8
 	WaveCompleted,
 	AllWavesDone,
 	PlayerDied
+};
+
+USTRUCT(BlueprintType)
+struct FKlotoEnemyWaveSpawnerInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TSoftClassPtr<AKlotoEnemyCharacter> SoftEnemyClassToSpawn;
+
+	UPROPERTY(EditAnywhere)
+	int32 MinPerSpawnCount = 1;
+
+	UPROPERTY(EditAnywhere)
+	int32 MaxPerSpawnCount = 3;
+};
+
+USTRUCT(BlueprintType)
+struct FKlotoEnemyWaveSpawnTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TArray<FKlotoEnemyWaveSpawnerInfo> EnemyWaveSpawnerInfos;
+
+	UPROPERTY(EditAnywhere)
+	int32 TotalEnemyToSpawnThisWave = 1;
 };
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSurvivalGameModeChangedDelegate, EKlotoSurvivalGameModeState, CurrentState);
@@ -39,4 +68,7 @@ private:
 
 	UPROPERTY(BlueprintAssignable, BlueprintCallable)
 	FOnSurvivalGameModeChangedDelegate OnSurvivalGameModeChanged;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	UDataTable*	EnemyWaveSpawnerDataTable;
 };
