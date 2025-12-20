ddc053bd3bde8f723f21184ba3d420ee06173e28
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-12-20 11:43:09 +0800

    Wave Progression Logic

--- Source/Kloto/Private/GameModes/KlotoSurvivalGameMode.cpp
+++ Source/Kloto/Private/GameModes/KlotoSurvivalGameMode.cpp
@@ -6,13 +6,58 @@
 void AKlotoSurvivalGameMode::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	checkf(EnemyWaveSpawnerDataTable, TEXT("Forgot to assign a data table in Survival GameMode"));
+
+	SetCurrentSurvivalGameModeState(EKlotoSurvivalGameModeState::WaitSpawnNewWave);
+
+	TotalWavesToSpawn = EnemyWaveSpawnerDataTable->GetRowNames().Num();
 }
 
 void AKlotoSurvivalGameMode::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
+
+	if (CurrentSurvivalGameModeState == EKlotoSurvivalGameModeState::WaitSpawnNewWave)
+	{
+		TimePassSinceStart += DeltaTime;
+		if (TimePassSinceStart >= SpawnNewWaveWaitTime)
+		{
+			TimePassSinceStart = 0.f;
+			SetCurrentSurvivalGameModeState(EKlotoSurvivalGameModeState::SpawningNewWave);
+		}
+	}
+
+	if (CurrentSurvivalGameModeState == EKlotoSurvivalGameModeState::SpawningNewWave)
+	{
+		TimePassSinceStart += DeltaTime;
+		if (TimePassSinceStart >= SpawnNewWaveWaitTime)
+		{
+			//Handle Spawn New Enemies
+			TimePassSinceStart = 0.f;
+			SetCurrentSurvivalGameModeState(EKlotoSurvivalGameModeState::InProgress);
+		}
+	}
+
+	if (CurrentSurvivalGameModeState == EKlotoSurvivalGameModeState::WaveCompleted)
+	{
+		TimePassSinceStart += DeltaTime;
+		if (TimePassSinceStart >= WaveCompletedWaitTime)
+		{
+			TimePassSinceStart = 0.f;
+
+			CurrentWaveCount++;
+
+			if (HasFinishedAllWaves())
+			{
+				SetCurrentSurvivalGameModeState(EKlotoSurvivalGameModeState::AllWavesDone);
+			}
+			else
+			{
+				SetCurrentSurvivalGameModeState(EKlotoSurvivalGameModeState::WaitSpawnNewWave);
+			}
+		}
+	}
 }
 
 void AKlotoSurvivalGameMode::SetCurrentSurvivalGameModeState(EKlotoSurvivalGameModeState InState)
@@ -20,4 +65,9 @@ void AKlotoSurvivalGameMode::SetCurrentSurvivalGameModeState(EKlotoSurvivalGameM
 	CurrentSurvivalGameModeState = InState;
 
 	OnSurvivalGameModeChanged.Broadcast(CurrentSurvivalGameModeState);
+}
+
+bool AKlotoSurvivalGameMode::HasFinishedAllWaves() const
+{
+	return CurrentWaveCount >= TotalWavesToSpawn;
 }
